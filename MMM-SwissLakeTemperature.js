e47ddeb73b88f47b2c3c4d319da44e6a71360184
roufri <rouven.fries@gmx.ch>
2020-11-04 19:44:56 +0100

    add files initially

---
+++ MMM-SwissLakeTemperature.js
@@ -0,0 +1,83 @@
+Module.register("MMM-SwissLakeTemperature", {
+	defaults: {
+		// default Coordinates are pointing to Zurich Bellevue
+		xCoordinate: '683474',
+		yCoordinate: '246769',
+		lake: 'zurich',
+		depth: '1'
+	},
+	getStyles: function () {
+		return ["custom.css", "font-awesome.css"];
+	},
+	start: function () {
+		this.url = '';
+	},
+	getDom: function () {
+		var temperatureDiv = document.createElement("div");
+		temperatureDiv.id = "temperatureDiv";
+		temperatureDiv.innerHTML = "Loading water temperature...";
+
+		var xCoord = this.config.xCoordinate;
+		var yCoord = this.config.yCoordinate;
+		var lake = this.config.lake;
+		var depth = this.config.depth;
+		var endTime = Date.now();
+		var startTime = endTime - (1000 * 60 * 60); // 1 hour back
+
+		this.url = `http://meteolakes.ch/api/coordinates/${xCoord}/${yCoord}/${lake}/temperature/${startTime}/${endTime}/${depth}`;
+
+		return temperatureDiv;
+	},
+	notificationReceived: function (notification, payload, sender) {
+		switch (notification) {
+			case "DOM_OBJECTS_CREATED":
+				// initial data load
+				this.sendSocketNotification("LOAD_WATER_DATA", this.url);
+
+				// update every 3.25 hours
+				var time = setInterval(() => {
+					this.sendSocketNotification("LOAD_WATER_DATA", this.url);
+				}, (1000 * 60 * 60 * 3.25))
+				break;
+		}
+	},
+	socketNotificationReceived: function (notification, payload) {
+		switch (notification) {
+			case "LOADED_WATER_DATA":
+				var data = JSON.parse(payload);
+
+				var temperatureDiv = document.getElementById("temperatureDiv");
+				temperatureDiv.innerHTML = ""; // overwrite loading text
+
+				var symbol = document.createElement("span");
+				symbol.className = "symbol fas " + this.getSymbolClassForTemperature(data.temperature);
+
+				var text = document.createTextNode(data.temperature + "°C in " + data.depth + "m Tiefe");
+
+				temperatureDiv.appendChild(symbol);
+				temperatureDiv.appendChild(text);
+				break;
+		}
+	},
+
+	/**
+	* Get the temperature-dependent css-class (icon + color)
+	* https://fontawesome.com/icons?d=gallery&q=thermometer
+	*
+	* @param temperature in celsius
+	* @returns a String with the class-name
+	*/
+	getSymbolClassForTemperature: function (temperature) {
+		if (temperature >= 22) {
+			return "fa-thermometer-full temp-hot";
+		} else if (temperature >= 18) {
+			return "fa-thermometer-three-quarters temp-warm";
+		} else if (temperature >= 10) {
+			return "fa-thermometer-half temp-average";
+		} else if (temperature >= 5) {
+			return "fa-thermometer-quarter temp-cold";
+		} else {
+			return "fa-thermometer-empty temp-freezing";
+		}
+	}
+})
---
+++ node_helper.js
@@ -0,0 +1,41 @@
+const NodeHelper = require("node_helper");
+const request = require("request");
+
+module.exports = NodeHelper.create({
+	socketNotificationReceived: function (notification, payload) {
+		switch (notification) {
+			case "LOAD_WATER_DATA":
+				console.log("LOAD_WATER_DATA");
+				// send request to get csv from http://meteolakes.ch/#!/data
+				request(
+					{
+						url: payload,
+						encoding: null,
+						method: "GET"
+					},
+					(error, response, body) => {
+						if (!error) {
+							if (response.statusCode === 200) {
+								var waterData = body.toString('utf8').split('\n')[1].split(',');
+
+								var depth = Math.abs(parseFloat(waterData[0]));
+								var temperature = parseFloat(waterData[1]).toFixed(1);
+
+								var waterData = {
+									"temperature": temperature,
+									"depth": depth
+								}
+
+								this.sendSocketNotification("LOADED_WATER_DATA", JSON.stringify(waterData));
+							} else if (response.statusCode > 400) {
+								console.log("No file found at url " + payload);
+							}
+						} else {
+							console.log("Error for request at " + payload);
+						}
+					}
+				); // end request
+				break;
+		}
+	},
+})
